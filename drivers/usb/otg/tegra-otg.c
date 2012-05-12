c90c8e018bfea782fe40d2a9d12ba4df329d11d7
jessica <jessica_jong@asus.com>
2012-05-12 14:55:17 -0700

    usb: otg: clear the VBUS INT bit when ID status is detected
    The function tegra_otg_set_peripheral() is called on probing stage
    to apply the correct otg state on boot.
    However, when the otg cable is plugged in on boot, the VBUS flags
    are not correct leading to misinterpreation of states.
    For now, we will clear the VBUS INT flag to make the state correct.
    
    [Issue 6464161] USB host can not work if the device boots up with the
    USB host cable connected
    
    Change-Id: Iab6d9529fe9e7aa5e5f0552bc4fcee6b2a4e1c64

--- drivers/usb/otg/tegra-otg.c
+++ drivers/usb/otg/tegra-otg.c
@@ -341,6 +341,7 @@ static int tegra_otg_set_peripheral(struct otg_transceiver *otg,
 		val |= USB_VBUS_INT_STATUS;
 	} else if (!(val & USB_ID_STATUS)) {
 		val |= USB_ID_INT_STATUS;
+		val &= ~USB_VBUS_INT_STATUS;
 	} else {
 		val &= ~(USB_ID_INT_STATUS | USB_VBUS_INT_STATUS);
 	}
