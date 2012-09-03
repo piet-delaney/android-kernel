0e5e26cebd25ae128c403979a9dd3c73caef9a4d
Will Tisdale <willtisdale@gmail.com>
2012-09-03 10:33:15 +0100

    tegra: pm: Disable switch to LP cluster at suspend

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -1037,15 +1037,15 @@ static struct kobject *suspend_kobj;
 static int tegra_pm_enter_suspend(void)
 {
 	pr_info("Entering suspend state %s\n", lp_state[current_suspend_mode]);
-	if (current_suspend_mode == TEGRA_SUSPEND_LP0)
-		tegra_lp0_cpu_mode(true);
+//	if (current_suspend_mode == TEGRA_SUSPEND_LP0)
+//		tegra_lp0_cpu_mode(true);
 	return 0;
 }
 
 static void tegra_pm_enter_resume(void)
 {
-	if (current_suspend_mode == TEGRA_SUSPEND_LP0)
-		tegra_lp0_cpu_mode(false);
+//	if (current_suspend_mode == TEGRA_SUSPEND_LP0)
+//		tegra_lp0_cpu_mode(false);
 	pr_info("Exited suspend state %s\n", lp_state[current_suspend_mode]);
 }
 
